6d4af63273c0f18320c3aa6232c77c82f7104a6e
leandro estrada <leandroestrada@MacBook-Pro-de-leandro.local>
2024-07-20 23:30:24 -0300

    Starting commit, adding main files

---
+++ UberCloneApp/UberClone/Core/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  UberClone
+//
+//  Created by leandro estrada on 20/07/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ UberCloneApp/UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by leandro estrada on 20/07/24.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
